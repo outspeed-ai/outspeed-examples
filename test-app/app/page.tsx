f2a77538d3294ac9d1e5089d3782217217dfa0a8
janak2 <janakagrawal1309@gmail.com>
2024-10-24 12:21:48 -0700

    fix datachannel bug

--- test-app/app/page.tsx
+++ test-app/app/page.tsx
@@ -1,45 +1,58 @@
-"use client"
-import { RealtimeAudio, RealtimeAudioVisualizer, useWebRTC } from "@outspeed/react";
+"use client";
+import {
+  RealtimeAudio,
+  RealtimeAudioVisualizer,
+  useWebRTC,
+} from "@outspeed/react";
+import { ConsoleLogger, createConfig } from "@outspeed/core";
 
 const OutspeedUI = ({ connection }) => {
-    return (<>
-    <RealtimeAudio track={connection.getRemoteAudioTrack()} />
-    <button onClick={() => connection.getLocalAudioTrack().pause()}>mute</button>
-    <button onClick={() => connection.getLocalAudioTrack().resume()}>unmute</button>
-    <button onClick={() => connection.dataChannel.send("hello")}>send message </button>
-    </>)
-}
+  return (
+    <>
+      <RealtimeAudio track={connection.getRemoteAudioTrack()} />
+      <button onClick={() => connection.getLocalAudioTrack().pause()}>
+        mute
+      </button>
+      <button onClick={() => connection.getLocalAudioTrack().resume()}>
+        unmute
+      </button>
+      <button onClick={() => connection.dataChannel.send("hello")}>
+        send message{" "}
+      </button>
+    </>
+  );
+};
 
 export default function Page() {
   const connection = useWebRTC({
-    config: {
-      functionURL: "https://infra.outspeed.com/run/58179982af68b222891be12b9a2b356d",
-      audio: {
-        deviceId: "",
+    config: createConfig({
+      functionURL: "http://0.0.0.0:8080",
+      audioDeviceId: "",
+      audioConstraints: {
         echoCancellation: true,
       },
-    },
+    }),
   });
 
   const connectToOutspeed = () => {
-    connection.connect()
-  }
+    connection.connect();
+  };
 
   const disconnectFromOutspeed = () => {
-    connection.disconnect()
-  }
+    connection.disconnect();
+  };
 
   return (
     <div>
       <h1>Hello, Next.js!</h1>
       <div>
-        {connection.connectionStatus === 'Connected' ? (
+        {connection.connectionStatus === "Connected" ? (
           <button onClick={disconnectFromOutspeed}>Disconnect</button>
         ) : (
           <button onClick={connectToOutspeed}>Connect</button>
         )}
-        {connection.connectionStatus === 'Connected' && (
-            <OutspeedUI connection={connection}/>
+        {connection.connectionStatus === "Connected" && (
+          <OutspeedUI connection={connection} />
         )}
       </div>
     </div>
