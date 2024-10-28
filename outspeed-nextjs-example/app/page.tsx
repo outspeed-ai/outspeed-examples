833e538d418de92590dca965b26706782a2c136c
janak2 <janakagrawal1309@gmail.com>
2024-10-28 10:15:19 -0700

    Update page.tsx

--- outspeed-nextjs-example/app/page.tsx
+++ outspeed-nextjs-example/app/page.tsx
@@ -11,7 +11,6 @@ export default function LandingPage() {
     config: {
       functionURL: string;
       audioDeviceId?: string;
-      videoDeviceId?: string;
     },
     targetURL: string
   ) {
