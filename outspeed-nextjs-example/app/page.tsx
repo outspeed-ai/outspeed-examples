fc20b79f78bbc069043901c299db56db04655fbf
janak2 <janakagrawal1309@gmail.com>
2024-10-27 18:21:43 -0700

    Revert "Update page.tsx"
    This reverts commit 6b9e47e0aa6bcca5a479fd3d163aff286698229b.

--- outspeed-nextjs-example/app/page.tsx
+++ outspeed-nextjs-example/app/page.tsx
@@ -22,30 +22,31 @@ export default function LandingPage() {
   }
 
   return (
-    <>
-      <div className="mt-2 justify-start p-4">
-        <a href="https://outspeed.com">
-          <Image
-            height={40}
-            width={200}
-            alt="logo"
-            src="/outspeed.svg"
-            className="h-10"
-          />
-        </a>
-      </div>
-      <div className="flex w-dvw flex-col items-center md:items-stretch md:flex-row">
-        <div className="flex-1 flex w-full justify-center bg-background">
-          <div className="flex-1 flex flex-col max-w-lg justify-center items-center md:px-10 md:max-w-2xl p-4">
-            <div className="mb-4 p-4 text-red-500 text-lg border border-red-500 rounded">
-              Please ensure that the app is running and Function URL is correct.
-            </div>
-            <WebRTCTakeInput
-              onSubmit={(config) => onSubmit(config, "/webrtc")}
-            />
+    <div className="flex h-dvh w-dvw flex-col items-center md:items-stretch md:flex-row">
+      <div className="flex-1 bg-[hsl(204,80%,5%)] w-full flex justify-center md:justify-end">
+        <div className="flex-1 p-4 flex flex-col max-w-lg md:max-w-2xl">
+          {/* Logo */}
+          <div className="mt-10 flex justify-start">
+            <a href="https://outspeed.com">
+              <Image
+                height={40}
+                width={200}
+                alt="logo"
+                src="/outspeed.svg"
+                className="h-10"
+              />
+            </a>
           </div>
         </div>
       </div>
-    </>
+      <div className="flex-1 flex w-full justify-center md:justify-start bg-background">
+        <div className="flex-1 flex flex-col max-w-lg justify-center md:px-10 md:max-w-2xl p-4">
+          <div className="mb-4 p-4 text-red-500 text-lg border border-red-500 rounded">
+            Please ensure that the app is running and Function URL is correct.
+          </div>
+          <WebRTCTakeInput onSubmit={(config) => onSubmit(config, "/webrtc")} />{" "}
+        </div>
+      </div>
+    </div>
   );
 }
