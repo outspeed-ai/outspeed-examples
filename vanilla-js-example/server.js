dd6c9b6683cdfa54bfab22179d20ba107f875847
Ankit Kumar <ankits.kvmdam@hotmail.com>
2024-11-05 20:00:52 +0530

    fix server

--- vanilla-js-example/server.js
+++ vanilla-js-example/server.js
@@ -17,18 +17,6 @@ const server = http.createServer((req, res) => {
     case ".js":
       contentType = "text/javascript";
       break;
-    case ".css":
-      contentType = "text/css";
-      break;
-    case ".json":
-      contentType = "application/json";
-      break;
-    case ".png":
-      contentType = "image/png";
-      break;
-    case ".jpg":
-      contentType = "image/jpeg";
-      break;
   }
 
   // Check if the file exists
